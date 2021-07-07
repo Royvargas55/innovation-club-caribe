619b1fc5d4f1a41535af94f11def01155e068da7
Royvargas55 <royvargas55@gmail.com>
2021-07-06 19:33:20 -0600

    Initial Commit

---
+++ src/containers/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// Styles
+import '../styles/components/Home.scss'
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className='title'>Innovation Club - Caribe</h1>            
+        </div>
+    )
+}
+
+export default Home;
---
+++ src/routes/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+// Components
+import Layout from '../components/Layout';
+import Home from '../containers/Home';
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Layout>
+              <Switch>
+                <Route exact path="/" component={Home} />
+              </Switch>
+            </Layout>
+        </BrowserRouter>
+    )
+}
+
+export default App;
