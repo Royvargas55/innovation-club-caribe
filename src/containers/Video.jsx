0458c4e232bbfed3a82a134ca9dd124714b5103c
--replace-all <Andre>
2021-07-21 20:51:12 -0600

    fixes to video page

--- src/containers/Video.jsx
+++ src/containers/Video.jsx
@@ -1,6 +1,6 @@
-import React, {useState, useRef, useEffect} from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { usePageVisibility } from '../hooks/usePageVisibility';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 // Components
 import Button from '../components/Button/Button';
@@ -9,34 +9,45 @@ import RailsSVG from '../components/RailsSVG';
 import '../styles/components/Video.scss';
 
 const Video = () => {
-    const videoPlayer = useRef(null);
-    const [showButton, setShowButton] = useState(false);
-    const isVisible = usePageVisibility();
+  const videoPlayer = useRef(null);
+  const [showButton, setShowButton] = useState(false);
+  const isVisible = usePageVisibility();
 
-    useEffect(() => {
-        isVisible === false ? videoPlayer.current.pause() : videoPlayer.current.play()
-    }, [isVisible]);
+  useEffect(() => {
+    videoPlayer.current.playing
+      ? isVisible === false
+        ? videoPlayer.current.pause()
+        : videoPlayer.current.play()
+      : '';
+  }, [isVisible]);
 
-    return (
-        <>
-            <div className='video'>
-                <h1 className='video__title'>Termina el Video Para Continuar</h1>
-                <video
-                    className='video__player'
-                    ref={videoPlayer}
-                    onEnded={() => {setShowButton(true)}} 
-                    width='1035px' 
-                    height='503px'
-                    controls
-                    src='../assets/video.mp4' type='video/mp4'>
-                </video>
-                {showButton && <Link to='/data-page'><Button text='Iniciar' /></Link>}
-                <div className='video__rails'>
-                    <RailsSVG />
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="video">
+        <h1 className="video__title">Termina el Video Para Continuar</h1>
+        <video
+          className="video__player"
+          ref={videoPlayer}
+          onEnded={() => {
+            setShowButton(true);
+          }}
+          width="1035px"
+          height="503px"
+          controls
+          src="../assets/video.mp4"
+          type="video/mp4"
+        ></video>
+        {showButton && (
+          <Link to="/data-page">
+            <Button text="Iniciar" />
+          </Link>
+        )}
+        <div className="video__rails">
+          <RailsSVG />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Video;
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 // Components
@@ -7,18 +7,30 @@ import Home from '../containers/Home';
 import Video from '../containers/Video';
 import DataPage from '../containers/DataPage';
 
+// Prototype configuration
+Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
+  get: function () {
+    return !!(
+      this.currentTime > 0 &&
+      !this.paused &&
+      !this.ended &&
+      this.readyState > 2
+    );
+  },
+});
+
 const App = () => {
-    return (
-        <BrowserRouter>
-            <Layout>
-              <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/video" component={Video} />
-                <Route exact path="/data-page" component={DataPage} />
-              </Switch>
-            </Layout>
-        </BrowserRouter>
-    )
-}
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/video" component={Video} />
+          <Route exact path="/data-page" component={DataPage} />
+        </Switch>
+      </Layout>
+    </BrowserRouter>
+  );
+};
 
 export default App;
